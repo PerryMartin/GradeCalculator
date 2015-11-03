7cb97caca1f04cf5a69a11b012ff1cfa14e899cf
mrcoxall <mr.coxall@mths.ca>
2015-11-03 14:33:43 -0500

    v1.0

---
+++ GradeCalculator/Calculator.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GradeCalculator
+{
+    partial class frmGardeCal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEntGrade = new System.Windows.Forms.Label();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.btnCalculater = new System.Windows.Forms.Button();
+            this.lblOuput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEntGrade
+            // 
+            this.lblEntGrade.AutoSize = true;
+            this.lblEntGrade.Location = new System.Drawing.Point(98, 11);
+            this.lblEntGrade.Name = "lblEntGrade";
+            this.lblEntGrade.Size = new System.Drawing.Size(62, 16);
+            this.lblEntGrade.TabIndex = 0;
+            this.lblEntGrade.Text = "Enter Grade";
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(85, 49);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(100, 21);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // btnCalculater
+            // 
+            this.btnCalculater.Location = new System.Drawing.Point(101, 104);
+            this.btnCalculater.Name = "btnCalculater";
+            this.btnCalculater.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculater.TabIndex = 2;
+            this.btnCalculater.Text = "Calculator";
+            this.btnCalculater.UseVisualStyleBackColor = true;
+            this.btnCalculater.Click += new System.EventHandler(this.btnCalculater_Click);
+            // 
+            // lblOuput
+            // 
+            this.lblOuput.AutoSize = true;
+            this.lblOuput.Location = new System.Drawing.Point(123, 169);
+            this.lblOuput.Name = "lblOuput";
+            this.lblOuput.Size = new System.Drawing.Size(0, 16);
+            this.lblOuput.TabIndex = 3;
+            // 
+            // frmGardeCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 226);
+            this.Controls.Add(this.lblOuput);
+            this.Controls.Add(this.btnCalculater);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.lblEntGrade);
+            this.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmGardeCal";
+            this.Text = "Grade Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEntGrade;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button btnCalculater;
+        private System.Windows.Forms.Label lblOuput;
+    }
+}
+
---
+++ GradeCalculator/Calculator.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 03-11-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#4-04
+ * This program displays Grade in percent
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeCalculator
+{
+    public partial class frmGardeCal : Form
+    {
+        private int GradeCalculat(string grade)
+        {
+            int percentGrade = 0;
+
+            if (grade == "NE")
+            {
+                percentGrade = 0;
+            }
+            else if (grade == "R")
+            {
+                percentGrade = 30;
+            }
+            else if (grade == "1-")
+            {
+                percentGrade = 52;
+            }
+            else if (grade == "1")
+            {
+                percentGrade = 55;
+            }
+            else if (grade == "1+")
+            {
+                percentGrade = 57;
+            }
+            else if (grade == "2-")
+            {
+                percentGrade = 62;
+            }
+            else if (grade == "2")
+            {
+                percentGrade = 65;
+            }
+            else if (grade == "2+")
+            {
+                percentGrade = 67;
+            }
+            else if (grade == "3-")
+            {
+                percentGrade = 72;
+            }
+            else if (grade == "3")
+            {
+                percentGrade = 75;
+            }
+            else if (grade == "3+")
+            {
+                percentGrade = 77;
+            }
+            else if (grade == "4-")
+            {
+                percentGrade = 85;
+            }
+            else if (grade == "4")
+            {
+                percentGrade = 90;
+            }
+            else if (grade == "4+")
+            {
+                percentGrade = 95;
+            }
+            else
+            {
+                percentGrade = -9999;
+            } 
+
+            return percentGrade;
+        }
+
+        public frmGardeCal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculater_Click(object sender, EventArgs e)
+        {
+            string gradeInput = this.txtGrade.Text;
+            int percentGrade;
+
+            percentGrade = GradeCalculat(gradeInput);
+
+            this.lblOuput.Text = Convert.ToString(percentGrade);
+        }
+    }
+}
